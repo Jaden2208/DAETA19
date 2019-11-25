8b3f148520f1aa38545ab015f6cdf5ad061d1781
yujeong <dldbwjd260@naver.com>
2019-11-25 19:19:18 +0900

    프로필 작성 클래스, 액티비티 만들었다.

--- app/src/main/java/com/example/daeta19/InquireActivity.java
+++ app/src/main/java/com/example/daeta19/InquireActivity.java
@@ -10,6 +10,7 @@ public class InquireActivity extends AppCompatActivity {
 
     private ImageButton back_btn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/daeta19/MyProfileFragment.java
+++ app/src/main/java/com/example/daeta19/MyProfileFragment.java
@@ -1,14 +1,18 @@
 package com.example.daeta19;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MyProfileFragment extends Fragment {
+
+    private Button send_btn;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -16,11 +20,16 @@ public class MyProfileFragment extends Fragment {
 
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+
     }
 
 }
+
