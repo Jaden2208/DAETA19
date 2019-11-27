9d8982f97f924da8a50462023cd0f11f61eaa00a
kimdonggyu2008 <dkkim2008@naver.com>
2019-11-27 15:05:08 +0900

    문의하기 전송버튼 활성화

--- app/src/main/java/com/example/daeta19/InquireActivity.java
+++ app/src/main/java/com/example/daeta19/InquireActivity.java
@@ -2,25 +2,40 @@ package com.example.daeta19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class InquireActivity extends AppCompatActivity {
 
-    private ImageButton back_btn;
-
+    private ImageButton btn_back;
+    private Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inquire);
 
-        back_btn = (ImageButton) findViewById(R.id.back_btn);
-        back_btn.setOnClickListener(new View.OnClickListener() {
+        btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        btn_send = (Button) findViewById(R.id.btn_send);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InquireActivity.this, "해당 내용이 문의되었습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(InquireActivity.this, SettingActivity.class);
+                startActivity(intent);
             }
         });
     }
